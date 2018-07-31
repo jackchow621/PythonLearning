d6432186da6d946cacbccfed4e9c3b871102ec33
jackchow <jack_chow621@sina.com>
2018-07-31 21:39:03 +0800

    create a remote python responsitory

---
+++ config.py
@@ -0,0 +1,11 @@
+MONGO_URL='192.168.1.99'
+MONGO_DB='toutiao'
+MONGO_TABLE='toutiao'
+
+GROUP_START=1
+GROUP_END=20
+
+KEYWORD='街拍'
+
+MONGO_DB2 = 'taobao'
+MONGO_TABLE2 = 'meishi'
---
+++ food.py
@@ -0,0 +1,90 @@
+import re
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pyquery import PyQuery as pq
+from config import *
+import pymongo
+
+
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB2]
+
+
+browser = webdriver.Chrome()
+wait = WebDriverWait(browser, 10)
+
+
+def search():
+    try:
+        browser.get('https://www.taobao.com/')
+        tb_input = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#q'))
+        )
+        search_btn = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button'))
+        )
+        tb_input.send_keys('美食')
+        search_btn.click()
+        total = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total'))
+        )
+        get_products()
+        return total.text
+    except TimeoutException:
+        return search()
+
+
+def next_page(page_number):
+    try:
+        print('正在翻页', page_number)
+        input = wait.until( EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > input")) )
+        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit")))
+        input.clear()
+        input.send_keys(page_number)
+        submit.click()
+        wait.until( EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'), str(page_number)))
+        get_products()
+    except TimeoutException:
+        print("------翻页失败------")
+        next_page(page_number)
+
+
+def get_products():
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item')))
+    html=browser.page_source
+    doc=pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()#items 方法返回对象列表
+    for item in items:
+        product={
+            'image':item.find('.pic .img').attr('src'),
+            'price':item.find('.price').text(),
+            'deal':item.find('.deal-cnt').text()[:-3],
+            'title':item.find('.title').text(),
+            'shop':item.find('.shop').text(),
+            'location':item.find('.location').text()
+        }
+        print(product)
+        save_to_mongoDB(product)
+
+
+def save_to_mongoDB(product):
+    try:
+        if db[MONGO_TABLE2].insert(product):
+            print('存储到MongoDB成功', product)
+    except Exception:
+        print('存储到MongoDB失败', product)
+
+
+def main():
+    total = search()
+    total = int(re.compile('(\d+)').search(total).group(1))
+    for i in range(2,total+1):
+        next_page(i)
+    browser.close()
+
+
+if __name__ == '__main__':
+    main()
